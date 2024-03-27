6aa62c1ab7b683efb48cf426a8bdcb36f83bd6c8
samuelfrnk <samuelandreas.frank@uzh.ch>
2024-03-27 10:41:09 +0100

    removed single quotes

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container);
