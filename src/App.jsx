4c0dd14ff007cf3b1452015ee6cc4548afe110c0
samuelfrnk <samuelandreas.frank@uzh.ch>
2024-03-28 13:34:56 +0100

    added template for websockets #27

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from "react";
 import AppRouter from "./components/router/AppRouter";
 import "./styles/global.scss";
-
+import React, { createContext, useEffect, useState } from "react";
+import { getWsUrl } from "./utils/getDomain";
+import { Client } from '@stomp/stompjs';
+import StandardErrorBoundary from "./utils/ErrorBoundary";
+require("./utils/websockets");
+export const WebSocketContext = createContext(null);
 const App = () => {
-  return(
-    <div className="h-dvh w-screen">
-      <AppRouter />
-    </div>
+
+    const [connections, setConnections] = useState({
+        stompConnection: new Client({
+            brokerURL: getWsUrl() + '/game'
+        })
+    })
+    useEffect(() => {
+        //Here we activate the stomp connection only needed to call once.
+        connections.stompConnection.activate();
+    }, [connections.stompConnection])
+
+    return(
+        <WebSocketContext.Provider value={connections}>
+            <div>
+                <StandardErrorBoundary>
+                    <AppRouter />
+                </StandardErrorBoundary>
+            </div>
+        </WebSocketContext.Provider>
   );
 };
 
