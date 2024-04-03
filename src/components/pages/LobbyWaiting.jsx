9325136cc03204aadd832c767406a11a4a5f884b
Kusi <markus.senn@execed.uzh.ch>
2024-04-03 20:54:25 +0200

    settings layout for #21

--- src/components/pages/LobbyWaiting.jsx
+++ src/components/pages/LobbyWaiting.jsx
@@ -3,57 +3,84 @@ import { Navigate, useNavigate, useParams } from "react-router-dom";
 import Header from "../ui/Header";
 import LobbySettings from "../ui/LobbySettings";
 import SxyButton from "../ui/SxyButton";
+import { api, handleError } from "../../utils/api";
 import "../../styles/Hero.scss";
+import useFeedback from "../../hooks/useFeedback";
 
 const LobbyWaiting = () => {
   const navigate = useNavigate();
+  const feedback = useFeedback()
   const pin = useParams().id;
   const [sessionToken, setSessionToken] = useState("");
   const [showSettings, setShowSettings] = useState(false);
-  const [isGameMaster, setIsGameMaster] = useState(false);
+  const [isGameMaster, setIsGameMaster] = useState(true);
 
   const [players, setPlayers] = useState([
     { name: "Player 1", features: ["/assets/Ava1.jpg", "Samuel", "+5", "20"] },
     { name: "Player 2", features: ["/assets/Ava2.jpg", "Elia", "+2", "18"] },
   ]);
 
+  const leaveLobby = async () => {
+    try{
+      const headers = { "Authorization": localStorage.getItem("token") };
+      // const response = api.put("/lobby/users/leave", {}, { headers });
+
+      navigate("/lobby");
+    } catch(error){
+      feedback.give(handleError(error), 3000, "error");
+    }
+  }
+
 
   return (
     <>
       {!sessionToken ?
         <>
-          <Header leave />
-          <div className="bg-neutral-400 flex flex-col" id="hero">
+          <Header />
+          <div className="bg-neutral-400 flex flex-col relative" id="hero">
             <div className="bg-neutral-100 max-w-sexy p-10 mb-10 shadow-md rounded-lg">
               <h1 className="font-semibold text-center mb-3 text-2xl">PIN: <b>{pin}</b></h1>
+              {/* TODO: switch to actual dynamic host name from lobby information */}
               <h1 className="font-semibold text-center text-2xl"><b>{localStorage.getItem("username")}</b> is the host</h1>
-              <div className="flex flex-col h-36 m-4 justify-evenly items-center">
+              <div className="flex flex-col h-44 mx-4 mt-2 justify-evenly items-center">
                 <SxyButton
                   text="Start Game"
                   color={"#72171D"}
                   func={() => {}}
-                  disabled={players.length < 1 && isGameMaster}
+                  disabled={players.length < 2 || !isGameMaster}
                   width="120px"
                 />
+
+                {isGameMaster ?
+                  <SxyButton
+                    text="Settings"
+                    color={"#72171D"}
+                    func={() => setShowSettings(true)}
+                    width="120px"
+                  />
+                  : null
+                }
+
                 <SxyButton
-                  text="Settings"
-                  color={"#72171D"}
-                  func={() => setShowSettings(true)}
+                  text="Leave Lobby"
                   width="120px"
+                  color={"#72171D"}
+                  func={leaveLobby}
                 />
               </div>
             </div>
-            <div className="bg-neutral-400 w-96 h-96 grid grid-cols-3 gap-4 content-center rounded-lg shadow-md" >
-
+            <div className="bg-neutral-400 w-96 h-96 rounded-lg shadow-md" >
+              <h1 className="text-center mt-4">Your favorite players soon in here</h1>
             </div>
+
+            {showSettings ?
+              <LobbySettings out={setShowSettings} />
+              : null 
+            }
           </div>
-          {showSettings ?
-            <LobbySettings />
-            : null 
-          }
         </>
         // session guard
-        : <Navigate to={`/game/${sessionToken}`} />}
+        : <Navigate to={`/game/${pin}`} />}
     </>
   );
 };
--- src/components/ui/LobbySettings.jsx
+++ src/components/ui/LobbySettings.jsx
@@ -1,12 +1,31 @@
 import React from "react";
-import "../../styles/Header.scss";
+import "../../styles/Hero.scss";
+import SxyButton from "./SxyButton";
 
-const LobbySettings = () => {
+const LobbySettings = ({out}) => {
 
+  const saveSettings = () => {
+
+  }
 
   return(
-    <div className="flex justify-between px-8 py-6 items-center bg-brand-500" id="header">
+    <div className="absolute flex flex-col p-10 items-center justify-between bg-brand-500" id="settings">
+      <h1 className="text-center text-5xl font-bold">Settings</h1>
 
+      <div className="flex w-60 justify-between">
+        <SxyButton
+          text="Save"
+          func={() => saveSettings}
+          width="75px"
+          color={"#72171D"}
+        />
+        <SxyButton
+          text="Cancel"
+          func={() => out()}
+          width="75px"
+          color={"red"}
+        />
+      </div>
     </div>
   );
 };
